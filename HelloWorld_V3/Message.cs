31e01130432bc2ec13acaedfdeea778a95c9c8eb
STEPHEN THOMPSON <2158116@student.swin.edu.au>
2024-03-02 14:25:50 +1100

    First working layout.

---
+++ HelloWorld_V3/Message.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//----------------------------//
+//           Message          //
+//----------------------------//
+//       -_text: String       //
+//----------------------------//
+//   + Message(string txt)    //
+//          + Pring()         //
+//----------------------------//
+
+namespace HelloWorld_V3
+{
+    //Name of the class
+    internal class Message  
+    {
+        //instances: stuff the object knows
+        private string _text;
+
+        //methods
+        public Message(string text) //constructor
+        {
+            _text = text;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_text);
+        }
+    }
+}
--- HelloWorld_V3/Program.cs
+++ HelloWorld_V3/Program.cs
@@ -1,11 +1,49 @@
-﻿namespace HelloWorld_V3
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace HelloWorld_V3
 {
+    
     internal class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, Hello!");
+             
+
+            Message myMessage = new Message("Hello, World! Greetings from Message Object.");
+            myMessage.Print();
+
+            Console.WriteLine("Please enter your name:");
+            string yourName = Console.ReadLine();
+
+            List<Message> messages = new List<Message>();
+
+            messages.Add(new Message("Kon'nichiwa " + yourName));
+            messages.Add(new Message("Hola "  + yourName));
+            messages.Add(new Message("Nǐ hǎo " + yourName));
+            messages.Add(new Message("Bonjour "  + yourName));
+            messages.Add(myMessage);
+
+            if (yourName.ToLower() == "wilma")
+            {
+                messages[0].Print();
+            }
+            else if (yourName.ToLower() == "fred")
+            {
+                messages[1].Print();
+            }
+            else if (yourName.ToLower() == "bob")
+            {
+                messages[2].Print();
+            }
+            else if (yourName.ToLower() == "jessica")
+            {
+                messages[3].Print();
+            }
+            else
+            {
+                messages[4].Print();
+            }
         }
     }
 }
